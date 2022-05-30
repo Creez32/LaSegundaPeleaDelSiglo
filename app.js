46af2d71e342076adfc16079d1b15476fa824519
Comision17 <comision17formar@gmail.com>
2022-05-30 10:26:05 -0300

    subo los trabajos practicos

---
+++ app.js
@@ -0,0 +1,11 @@
+const process = require('process');
+let funcionesDeTareas = require('./funcionesDeTareas')
+
+switch (process.argv[2]) {
+    case "listar":
+        funcionesDeTareas.listar
+        break;
+
+    default:
+        break;
+}
